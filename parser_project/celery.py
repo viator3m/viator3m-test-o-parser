4d8b55732ecfd1e44af82728ae8ccd94dcaf972d
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-08-03 16:07:19 +0300

    feat(project): dockerize project

--- parser_project/celery.py
+++ parser_project/celery.py
@@ -3,6 +3,6 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parser_project.settings')
-app = Celery('parser_project', backend='rpc://', broker='amqp://localhost')
+app = Celery('parser_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
--- parser_project/settings.py
+++ parser_project/settings.py
@@ -5,15 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 # Application definition
 
@@ -65,17 +63,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'parser_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -95,7 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -109,11 +108,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -124,7 +123,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-
 # Users models
 
 AUTH_USER_MODEL = 'users.CustomUser'
@@ -134,7 +132,14 @@ AUTH_USER_MODEL = 'users.CustomUser'
 AMOUNT = 10
 START_URL = 'https://www.ozon.ru/seller/1/products/'
 
-
 # Celery settings
 
 CELERY_TASK_TRACK_STARTED = True
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_BACKEND')
+
+# Django superuser
+
+DJANGO_USER = os.getenv('DJANGO_SUPERUSER_USERNAME')
+DJANGO_EMAIL = os.getenv('DJANGO_SUPERUSER_EMAIL')
+DJANGO_PWD = os.getenv('DJANGO_SUPERUSER_PASSWORD')

69ec6ce5d4bdf173ba17d2f989e5717a477a82f8
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-08-04 11:21:04 +0300

    fix(docker): rename variable

--- parser_project/settings.py
+++ parser_project/settings.py
@@ -69,9 +69,9 @@ WSGI_APPLICATION = 'parser_project.wsgi.application'
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.mysql',
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "NAME": os.getenv('MYSQL_DATABASE'),
+        "USER": os.getenv('MYSQL_USER'),
+        "PASSWORD": os.getenv('MYSQL_PASSWORD'),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
     }

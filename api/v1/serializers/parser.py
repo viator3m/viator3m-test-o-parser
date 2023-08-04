5bd4a123c981dc320f598bb4bcfc944c9e4e86a4
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-08-04 10:24:39 +0300

    fix(parser): refactoring, change parameter name, add serializer for POST request

--- api/v1/serializers/parser.py
+++ api/v1/serializers/parser.py
@@ -3,6 +3,15 @@ from rest_framework import serializers
 from parser.models import Product
 
 
+class ProductCreateSerializer(serializers.Serializer):
+    products_count = serializers.IntegerField(
+        min_value=1,
+        max_value=50,
+        default=10,
+        required=False,
+    )
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
--- api/v1/views/parser.py
+++ api/v1/views/parser.py
@@ -1,42 +1,61 @@
 from celery.result import AsyncResult
+from django.conf import settings as conf
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 
-from api.v1.serializers.parser import ProductSerializer
+from api.v1.serializers.parser import ProductSerializer, \
+    ProductCreateSerializer
 from parser.models import Parsing, Product
 from parser.utils import parsing
 
 
 class ProductListView(ListCreateAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
+    def get_serializer_class(self):
+        match self.request.method:
+            case 'POST': return ProductCreateSerializer
+            case _: return ProductSerializer
+
+    @swagger_auto_schema(
+        responses={
+            200: openapi.Response(
+                description="Successful request",
+                examples={"application/json": {"info": "Parsing started"}}
+            ),
+        }
+    )
     def post(self, request, *args, **kwargs):
-        amount = request.data.get('amount', 10)
-        if not isinstance(amount, int):
-            raise ValidationError({
-                'error': f'Amount must be an integer,'
-                         f' not {amount.__class__.__name__}'
-            })
-        tasks = parsing.delay(amount)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            products_count = serializer.validated_data.get(
+                'products_count', 10
+            )
+            tasks = parsing.delay(products_count)
 
-        while True:
-            task_status = AsyncResult(tasks.task_id).status
-            match task_status:
-                case 'PENDING':
-                    continue
-                case 'STARTED':
-                    return Response(
-                        {'info': 'Parsing started'},
-                        status=status.HTTP_200_OK
-                    )
-                case 'FAILURE':
-                    return Response(
-                        {'info': 'Something went wrong'},
-                        status=status.HTTP_400_BAD_REQUEST
-                    )
+            while True:
+                task_status = AsyncResult(tasks.task_id).status
+                match task_status:
+                    case 'PENDING':
+                        continue
+                    case 'STARTED':
+                        return Response(
+                            {'info': 'Parsing started'},
+                            status=status.HTTP_200_OK
+                        )
+                    case 'FAILURE':
+                        return Response(
+                            {'info': 'Something went wrong'},
+                            status=status.HTTP_400_BAD_REQUEST
+                        )
+        return Response(
+            {'info': 'Something went wrong'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
     def get(self, request, *args, **kwargs):
         last_parsing = Parsing.objects.last()
